46470d200df01d0bba185b02537640adcfc56447
zoumiaojiang <zoumiaojiang@baidu.com>
2018-07-19 11:32:07 +0800

    add .npmignore

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const componentNormalizer = require('vue-loader/lib/runtime/componentNormalizer'
 const addStylesClient = require('vue-style-loader/lib/addStylesClient')
 const listToStyles = require('vue-style-loader/lib/listToStyles')
 
-const promise = require('babel-runtime/core-js/promise').default
+// const promise = require('babel-runtime/core-js/promise').default
 const symbol = require('babel-runtime/core-js/symbol').default
 const set = require('babel-runtime/core-js/set').default
 const arrayFrom = require('babel-runtime/core-js/array/from').default
-const objectAssign = require('babel-runtime/core-js/object/assign').default
-const objectCreate = require('babel-runtime/core-js/object/create').default
-const objectFreeze = require('babel-runtime/core-js/object/freeze').default
-const objectGetPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of').default
+// const objectAssign = require('babel-runtime/core-js/object/assign').default
+// const objectCreate = require('babel-runtime/core-js/object/create').default
+// const objectFreeze = require('babel-runtime/core-js/object/freeze').default
+// const objectGetPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of').default
 
 function mount (obj, name, host = window) {
   if (typeof host[name] === 'undefined') {
@@ -30,14 +30,14 @@ function mount (obj, name, host = window) {
   }
 }
 
-mount(promise, 'Promise')
+// mount(promise, 'Promise')
 mount(symbol, 'Symbol')
 mount(set, 'Set')
 mount(arrayFrom, 'from', Array)
-mount(objectAssign, 'assign', Object)
-mount(objectCreate, 'create', Object)
-mount(objectFreeze, 'freeze', Object)
-mount(objectGetPrototypeOf, 'getPrototypeOf', Object)
+// mount(objectAssign, 'assign', Object)
+// mount(objectCreate, 'create', Object)
+// mount(objectFreeze, 'freeze', Object)
+// mount(objectGetPrototypeOf, 'getPrototypeOf', Object)
 
 let helpers = {}
 
