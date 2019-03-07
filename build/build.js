9e6258adcb076fa9463e323bc8f627f4f98e4aa7
tanglei02 <tanglei02@baidu.com>
2019-03-07 19:44:10 +0800

    将 mip 核心和 cli 跟 external 相关的逻辑全部移到这个包来做统一管理

--- build/build.js
+++ build/build.js
@@ -5,17 +5,13 @@
 
 'use strict'
 
-process.env.NODE_ENV = 'production'
+// process.env.NODE_ENV = 'production'
 
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const glob = require('glob')
-// const rollup = require('rollup')
-// const cjs = require('rollup-plugin-commonjs')
-// const resolve = require('rollup-plugin-node-resolve')
-// const uglify = require('uglify-es')
-// const zlib = require('zlib')
-// const replace = require('rollup-plugin-replace')
+const {prefix} = require('../lib/data')
+const generate = require('../lib/generate')
 
 const root = path.resolve(__dirname, '..')
 
@@ -26,7 +22,7 @@ try {
   fs.mkdirSync(distDir, {recursive: true})
 } catch (e) {}
 
-const {imports, refs} = preHandleSourceFile()
+const {imports, vars, statements} = generate()
 
 const srcPaths = glob.sync('**/*', {
   cwd: srcDir,
@@ -35,64 +31,157 @@ const srcPaths = glob.sync('**/*', {
 
 srcPaths.forEach(srcPath => {
   let file = fs.readFileSync(path.resolve(srcDir, srcPath), 'utf-8')
-  file = file.replace('__INJECT_BABEL_RUNTIME_HELPERS_IMPORT__', imports)
-    .replace('__INJECT_BABEL_RUNTIME_HELPERS_REF__', refs)
+  file = file
+    .replace('__INJECT_PREFIX__', prefix)
+    .replace('__INJECT_BABEL_RUNTIME_HELPERS_IMPORT__', imports)
+    .replace('__INJECT_BABEL_RUNTIME_HELPERS_VARIABLES__', vars)
+    .replace('__INJECT_BABEL_RUNTIME_HELPERS_STATEMENTS__', statements)
   fs.writeFileSync(path.resolve(distDir, srcPath), file, 'utf-8')
 })
 
-let distPath = path.resolve(root, 'dist', 'mip-components-webpack-helpers.js')
+// @TEST
+try {
+  const nodeModules = path.resolve(root, '../minify/mip2/packages/mip/node_modules/mip-components-webpack-helpers')
+  fs.removeSync(path.resolve(nodeModules, 'dist'))
+  fs.removeSync(path.resolve(nodeModules, 'package.json'))
+  fs.copySync(path.resolve(root, 'dist'), path.resolve(nodeModules, 'dist'))
+  fs.copySync(path.resolve(root, 'package.json'), path.resolve(nodeModules, 'package.json'))
+}
+catch (e) {
+  console.log(e)
+}
+// @TEST
+
+// let distPath = path.resolve(root, 'dist', 'mip-components-webpack-helpers.js')
+
+// function preHandleSourceFile () {
+//   const babelHelpersPath = path.resolve(root, 'node_modules', 'babel-runtime', 'helpers')
+//   const babelHelpersFiles = fs.readdirSync(babelHelpersPath)
+
+//   let importHeader = ''
+//   let moduleRef = `var BABEL_RUNTIME_HELPERS = 'babel-runtime/helpers/'`
+
+//   babelHelpersFiles.forEach(file => {
+//     let fileName = file.replace('.js', '')
+//     let modulePath = 'babel-runtime/helpers/' + fileName
+//     let moduleName = '_' + fileName.replace(/-/g, '_')
+//     importHeader += `\nimport ${moduleName} from '${modulePath}'`
+//     moduleRef += `\nhelpers[BABEL_RUNTIME_HELPERS + '${fileName}'] = ${moduleName}`
+//   })
+
+//   return {
+//     imports: importHeader,
+//     refs: moduleRef
+//   }
+// }
+
 
 function preHandleSourceFile () {
-  const babelHelpersPath = path.resolve(root, 'node_modules', 'babel-runtime', 'helpers')
-  const babelHelpersFiles = fs.readdirSync(babelHelpersPath)
+  const helpers = [
+    // "babel-runtime/core-js/array/from",
+    // "babel-runtime/core-js/get-iterator",
+    // "babel-runtime/core-js/json/stringify",
+    // "babel-runtime/core-js/number/is-finite",
+    // "babel-runtime/core-js/object/assign",
+    // "babel-runtime/core-js/object/create",
+    // "babel-runtime/core-js/object/define-properties",
+    // "babel-runtime/core-js/object/get-own-property-descriptor",
+    // "babel-runtime/core-js/object/get-own-property-names",
+    // "babel-runtime/core-js/object/get-prototype-of",
+    // "babel-runtime/core-js/object/keys",
+    // "babel-runtime/core-js/promise",
+    // "babel-runtime/core-js/set",
+    "babel-runtime/helpers/asyncToGenerator",
+    "babel-runtime/helpers/classCallCheck",
+    "babel-runtime/helpers/createClass",
+    "babel-runtime/helpers/defineProperty",
+    "babel-runtime/helpers/extends",
+    "babel-runtime/helpers/get",
+    "babel-runtime/helpers/inherits",
+    "babel-runtime/helpers/possibleConstructorReturn",
+    "babel-runtime/helpers/slicedToArray",
+    "babel-runtime/helpers/toConsumableArray",
+    "babel-runtime/helpers/typeof",
+    "babel-runtime/regenerator",
+  ]
 
-  let importHeader = ''
-  let moduleRef = `var BABEL_RUNTIME_HELPERS = 'babel-runtime/helpers/'`
+  const map = [
+    {
+      origin: '@babel/runtime/helpers/esm/',
+      dist: 'babel-runtime/helpers/',
+      esm: true,
+      originIdentifierPrefix: '__AT_BABEL_RUNTIME_HELPERS_ESM_',
+      distIdentifierPrefix: '__BABEL_RUNTIME_HELPERS_'
+    },
+    {
+      origin: '@babel/runtime/regenerator/index',
+      dist: 'babel-runtime/regenerator',
+      esm: false
+    }
+  ]
 
-  babelHelpersFiles.forEach(file => {
-    let fileName = file.replace('.js', '')
-    let modulePath = 'babel-runtime/helpers/' + fileName
-    let moduleName = '_' + fileName.replace(/-/g, '_')
-    importHeader += `\nimport ${moduleName} from '${modulePath}'`
-    moduleRef += `\nhelpers[BABEL_RUNTIME_HELPERS + '${fileName}'] = ${moduleName}`
+  let mapper = helpers.map(helper => {
+    for (let i = 0; i < map.length; i++) {
+      if (helper.indexOf(map[i].dist) === 0) {
+        let moduleName = '_' + helper.replace(/[-/]/g, '_')
+        let rest = helper.slice(map[i].dist.length)
+        let originPath = map[i].origin + rest
+        let restIdentifier = rest && `__STRING_OF_${rest.replace(/[-/]/g, '_')}__` || ''
+        let distIdentifier = map[i].distIdentifierPrefix && `${map[i].distIdentifierPrefix} + ${restIdentifier}` || `'${map[i].dist}'`
+        let originIdentifier = map[i].originIdentifierPrefix && `${map[i].originIdentifierPrefix} + ${restIdentifier}` || `'${map[i].origin}'`
+        return {
+          imports: `import ${map[i].esm ? '* as' : ''} ${moduleName} from '${originPath}'`,
+          vars: rest && `var ${restIdentifier} = '${rest}'` || '',
+          statements: [
+            `helpers[${distIdentifier}] = ${moduleName}`,
+            `helpers[${originIdentifier}] = ${moduleName}`
+          ].join('\n')
+        }
+      }
+    }
   })
 
   return {
-    imports: importHeader,
-    refs: moduleRef
+    imports: mapper
+      .map(m => m.imports).join('\n'),
+    vars: mapper
+      .map(m => m.vars)
+      .concat(
+        map
+        .filter(m => m.originIdentifierPrefix || m.distIdentifierPrefix)
+        .map(m => {
+          let result = ''
+          if (m.originIdentifierPrefix) {
+            result += `var ${m.originIdentifierPrefix} = '${m.origin}'\n`
+          }
+          if (m.distIdentifierPrefix) {
+            result += `var ${m.distIdentifierPrefix} = '${m.dist}'\n`
+          }
+          return result
+        })
+      )
+      .join('\n'),
+    statements: mapper
+      .map(m => m.statements).join('\n'),
   }
+  // const babelHelpersPath = path.resolve(root, 'node_modules', 'babel-runtime', 'helpers')
+  // const babelHelpersFiles = fs.readdirSync(babelHelpersPath)
+
+  // let importHeader = ''
+  // let moduleRef = `var BABEL_RUNTIME_HELPERS = 'babel-runtime/helpers/'`
+
+  // babelHelpersFiles.forEach(file => {
+  //   let fileName = file.replace('.js', '')
+  //   let modulePath = 'babel-runtime/helpers/' + fileName
+  //   let moduleName = '_' + fileName.replace(/-/g, '_')
+  //   importHeader += `\nimport ${moduleName} from '${modulePath}'`
+  //   moduleRef += `\nhelpers[BABEL_RUNTIME_HELPERS + '${fileName}'] = ${moduleName}`
+  // })
+
+  // return {
+  //   imports: importHeader,
+  //   refs: moduleRef
+  // }
 }
 
-// function build () {
-//   let {imports, refs} = preHandleSourceFile()
 
-//   rollup.rollup({
-//     input: path.resolve(root, 'src/index.js'), // sourcePath,
-//     plugins: [
-//       replace({
-//         __INJECT_BABEL_RUNTIME_HELPERS_IMPORT__: imports,
-//         __INJECT_BABEL_RUNTIME_HELPERS_REF__: refs
-//       }),
-//       resolve(),
-//       cjs(
-//         {
-//           include: 'node_modules/**'
-//         }
-//       )
-//     ]
-//   })
-//   .then(bundle => bundle.write({
-//     file: distPath,
-//     format: 'es',
-//     name: 'installMipComponentsPolyfill'
-//   }))
-
-//     // .then(({ code }) => {
-//     //   console.log(code)
-//     //   // code = uglify.minify(code).code
-//     //   write(distPath, code, true)
-//     // })
-// }
-
-// // preHandleSourceFile()
-// build()
--- src/index.js
+++ src/index.js
@@ -5,61 +5,10 @@
 
 __INJECT_BABEL_RUNTIME_HELPERS_IMPORT__
 
-import regenerator from 'babel-runtime/regenerator'
-import cssBase from 'css-loader/lib/css-base'
-import componentNormalizer from 'vue-loader/lib/runtime/componentNormalizer'
-import addStylesClient from './add-styles-client'
-import listToStyles from 'vue-style-loader/lib/listToStyles'
+export default function () {
+  var helpers = window.__INJECT_PREFIX__ = []
 
-import symbol from 'babel-runtime/core-js/symbol'
-import set from 'babel-runtime/core-js/set'
-import arrayFrom from 'babel-runtime/core-js/array/from'
+  __INJECT_BABEL_RUNTIME_HELPERS_VARIABLES__
 
-// import promise from 'babel-runtime/core-js/promise'
-// import objectAssign from 'babel-runtime/core-js/object/assign'
-// import objectCreate from 'babel-runtime/core-js/object/create'
-// import objectFreeze from 'babel-runtime/core-js/object/freeze'
-// import objectGetPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of'
-
-function mount (obj, name, host) {
-  host = host || window
-  if (typeof host[name] === 'undefined') {
-    host[name] = obj
-  }
-}
-
-function esm(obj) {
-  return obj.__esModule ? obj : {__esModule: true, default: obj}
-}
-
-export default function installMipComponentsPolyfill () {
-  var helpers = {}
-  var cssLoaderKey = 'css-loader/lib/css-base'
-  var regeneratorKey = 'babel-runtime/regenerator'
-
-  __INJECT_BABEL_RUNTIME_HELPERS_REF__
-
-  helpers[cssLoaderKey] = cssBase
-  helpers[regeneratorKey] = regenerator
-  helpers['vue-loader/lib/runtime/componentNormalizer'] = componentNormalizer
-  helpers['vue-style-loader/lib/addStylesClient'] = addStylesClient
-  helpers['vue-style-loader/lib/listToStyles'] = listToStyles
-
-  // 兼容 mip-cli 里的 commonjs 写法。。
-  Object.keys(helpers).forEach(function (key) {
-    if (key !== regeneratorKey && key !== cssLoaderKey) {
-      helpers[key] = esm(helpers[key])
-    }
-  })
-
-  mount(symbol, 'Symbol')
-  mount(set, 'Set')
-  mount(arrayFrom, 'from', Array)
-  mount(helpers, '__mipComponentsWebpackHelpers__')
-
-  // mount(promise, 'Promise')
-  // mount(objectAssign, 'assign', Object)
-  // mount(objectCreate, 'create', Object)
-  // mount(objectFreeze, 'freeze', Object)
-  // mount(objectGetPrototypeOf, 'getPrototypeOf', Object)
+  __INJECT_BABEL_RUNTIME_HELPERS_STATEMENTS__
 }
